944aece8cd550586737f7a7c98c535a04a77981d
Kaixiang Ren <kaixiang@pop-os.localdomain>
2020-07-22 12:55:03 +1000

    Submit Order and Order Summary finished

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import classes from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+  {label: 'Salad', type: 'salad'},
+  {label: 'Bacon', type: 'bacon'},
+  {label: 'Cheese', type: 'cheese'},
+  {label: 'Meat', type: 'meat'}
+]
+
+const buildControls = (props) => (
+  <div className={classes.BuildControls}>
+    <p>Current price: <strong>AU$ {props.price.toFixed(2)}</strong></p>
+    {controls.map(c => {
+      return <BuildControl key={c.label}
+                           label={c.type}
+                           added={() => props.ingredientAdded(c.type)}
+                           removed={() => props.ingredientRemoved(c.type)}
+                           disabled={props.disabled[c.type]}/>
+    })}
+    <button className={classes.OrderButton} disabled={!props.purchasable} onClick={props.ordered}>ORDER NOW</button>
+  </div>
+);
+
+export default buildControls;
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,101 @@
+import React, {Component} from 'react';
+
+import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+import Modal from "../../components/UI/Modal/Modal";
+import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.5,
+  meat: 1,
+  bacon: 1
+}
+
+class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    },
+    totalPrice: 4,
+    purchasable: false,
+    purchasing: false
+  }
+
+  updatePurchaseState = (ingredients) => {
+    const sum = Object.keys(ingredients)
+      .map(igKey => {
+        return ingredients[igKey];
+      })
+      .reduce((sum, el) => {
+        return sum + el;
+      }, 0);
+    this.setState({
+      purchasable: sum > 0
+    });
+  }
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredient = {
+      ...this.state.ingredients
+    };
+    updatedIngredient[type] = updatedCount;
+    const newPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
+    this.setState({
+      ingredients: updatedIngredient,
+      totalPrice: newPrice
+    });
+    this.updatePurchaseState(updatedIngredient);
+  }
+
+  removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    let updatedCount = oldCount - 1;
+    updatedCount = updatedCount > 0 ? updatedCount : 0;
+    const updatedIngredient = {
+      ...this.state.ingredients
+    };
+    updatedIngredient[type] = updatedCount;
+    let newPrice = this.state.totalPrice - INGREDIENT_PRICES[type];
+    newPrice = newPrice > 4 ? newPrice : 4;
+    this.setState({
+      ingredients: updatedIngredient,
+      totalPrice: newPrice
+    });
+    this.updatePurchaseState(updatedIngredient);
+  }
+
+  purchaseHandler = () => {
+    const isPurchasing = this.state.purchasing;
+    this.setState({purchasing: !isPurchasing});
+  }
+
+  render() {
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
+    return (
+      <Aux>
+        <Modal show={this.state.purchasing}><OrderSummary ingredients={this.state.ingredients}/></Modal>
+        <Burger ingredients={this.state.ingredients}/>
+        <BuildControls ingredientAdded={this.addIngredientHandler}
+                       ingredientRemoved={this.removeIngredientHandler}
+                       price={this.state.totalPrice}
+                       purchasable={this.state.purchasable}
+                       ordered={this.purchaseHandler}
+                       disabled={disabledInfo}/>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;
